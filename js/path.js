8a778e76bfb1befe89c035a95b96f18d8e478ea3
ss-rin <sebasrinconrin@gmail.com>
2021-02-27 16:00:00 -0500

    Collision with body

---
+++ js/path.js
@@ -0,0 +1,136 @@
+class snakeTest
+{
+  constructor()
+  {
+    this.createMe();
+  }
+
+  myBody = $("<div>")
+  myPosX = 0;
+  myPosY = 0;
+
+  createMe()
+  {
+    this.myBody.attr("class", "test");
+    
+  }
+
+  setPos(x,y)
+  {
+    this.myBody.css({
+      "top": y,
+      "left": x
+    });
+    this.myPosX = x;
+    this.myPosY = y;
+    $(".container").append(this.myBody);      
+  }
+
+}
+
+function doSomething()
+{
+  console.log("Were working")
+}
+
+function loop(a, b)
+{
+  let x = a;
+  let y = b;
+
+
+  console.log(pressedKey)
+
+  if(velX == 10 || velX == -10)
+  {
+    if(pressedKey=="ArrowUp")
+    {
+      velX = 0;
+      velY = -10;
+    }
+    else if(pressedKey=="ArrowDown")
+    {
+      velX = 0;
+      velY = 10;
+    }
+  }
+
+  if(velY == 10 || velY == -10)
+  {
+    if(pressedKey=="ArrowLeft")
+    {
+      velY = 0;
+      velX = -10;
+    }
+    else if(pressedKey=="ArrowRight")
+    {
+      velY = 0;
+      velX = 10;
+    }
+  }
+  x += velX;
+  y += velY;
+
+  if(snakes.length>=1)
+  {
+    snakes[0].setPos(x, y);
+    path.unshift([snakes[0].myPosX, snakes[0].myPosY]);
+    if(snakes.length < path.length - 1)
+    {path.pop()}
+    
+    for(let elem of snakes)
+    {
+      elem.setPos(path[snakes.indexOf(elem)][0], path[snakes.indexOf(elem)][1])
+    }
+
+    for (let elem of path.slice(1, path.length + 1))
+    {
+      if( snakes[0].myPosX == elem[0] && snakes[0].myPosY == elem[1])
+      {
+        alert("Game over");
+        snakes = [];
+        path = [];
+        break
+      }
+    }
+    
+  }
+
+
+
+  console.log(path);
+  console.log(snakes);
+  doSomething();
+  setTimeout(
+    function(){
+      loop(x, y)
+    }, 100
+  )
+}
+
+function createSnake()
+{
+  snakes.unshift(new snakeTest)
+}
+
+function checkKey(e)
+{
+  pressedKey = e.code;
+  console.log(e.code);
+}
+
+
+let posX = 0;
+let posY = 0;
+let path = [];
+let snakes = [];
+let pressedKey = "ArrowDown";
+let velX = 10;
+let velY = 0;
+
+$(document).on("click", createSnake)
+$(document).on("keydown", checkKey)
+
+console.log("test");
+loop(velX, velY);
+
